d224da86b60eacec1c50f8612a6f93e5edaad9b3
Carlos Henrique <chenrilima.ch@gmail.com>
2022-07-19 05:56:07 -0300

    fix: fixing variables and add comments

---
+++ lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,20 @@
+void main() {
+  num anyNumber = 1;
+  num anyNumber2 = 1.5;
+  // variáveis do tipo num, podem ser tanto double quanto int, pois é uma classe herdada de int e double.
+
+  int idade = 28;
+
+  double valor = 3.400;
+
+  String curso;
+  curso = 'Novo Curso';
+
+  Object anyObject = 1.5;
+// variáveis do tipo Object, podem ser/receber qualquer tipo de valor
+
+  dynamic anyDynamic = 0.5;
+  anyDynamic = 'teste';
+  anyDynamic = 1.2;
+  // variáveis do tipo dynamic, podem ser/receber qualquer tipo de valor
+}
