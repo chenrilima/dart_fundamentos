7296e3fa583af19162231f78e4e3cee409cd217b
Carlos Henrique <chenrilima.ch@gmail.com>
2022-07-20 06:29:29 -0300

    feat: relacionais

---
+++ lib/3_operadores/2_operadores_relacionais.dart
@@ -0,0 +1,20 @@
+void main() {
+  final idade = 18;
+  final tipoPet = "Gato";
+
+  if (idade == 18) {
+    print("Pode tirar habilitação");
+  }
+
+  if (idade > 17) {
+    print("Pode tirar habilitação");
+  }
+
+  if (idade >= 18) {
+    print("Pode tirar habilitação");
+  }
+
+  if (tipoPet != 'Cachorro') {
+    print("Desculpe, não temos nada para o seu Pet");
+  }
+}
