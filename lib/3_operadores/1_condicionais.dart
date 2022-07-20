60c61a24da334f0fdc4b8d229a4ddf8b9c9429c0
Carlos Henrique <chenrilima.ch@gmail.com>
2022-07-20 06:29:17 -0300

    feat: condicionais

---
+++ lib/3_operadores/1_condicionais.dart
@@ -0,0 +1,11 @@
+void main() {
+  final idade = 18;
+
+  if (idade > 18) {
+    print("Já posso dirigir");
+  } else if (idade == 18) {
+    print("Já posso dirigir");
+  } else {
+    print('Não posso Dirigir');
+  }
+}
