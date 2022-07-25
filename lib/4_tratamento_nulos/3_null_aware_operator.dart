3ee45acff9457f7c1fc4a431c30441cfece6bd76
Carlos Henrique <chenrilima.ch@gmail.com>
2022-07-25 07:13:02 -0300

    chore: adding ! in comments

--- lib/4_tratamento_nulos/3_null_aware_operator.dart
+++ lib/4_tratamento_nulos/3_null_aware_operator.dart
@@ -4,11 +4,11 @@ void main() {
   var sobrenome = "Henrique";
   // var nomeCompleto = ((nome != null) ? '${nome!}Carlos' : 'Carlos Henrique');
   var nomeCompleto = (nome ?? 'Carlos ') + sobrenome;
-  // se nome for nulo, recebe Carlos e concatena com a variável sobre nome
+  // ! se nome for nulo, recebe Carlos e concatena com a variável sobrenome
 
   String? nomeCompleto2;
   print(nomeCompleto2 ??
-      'Carlos Henrique'); // se nomeCompleto2 for null, vai receber: 'Carlos Henrique'
+      'Carlos Henrique'); // ! se nomeCompleto2 for null, vai receber: 'Carlos Henrique'
 
   print(nomeCompleto);
 }
--- lib/4_tratamento_nulos/4_conditional_property_access.dart
+++ lib/4_tratamento_nulos/4_conditional_property_access.dart
@@ -1,11 +1,11 @@
 String? nomeCompleto;
 
 void main() {
-  // var nomeCompletoLocal = nomeCompleto  ?? 'Nome não prenchido'; // checando com null aware operator
+  // var nomeCompletoLocal = nomeCompleto  ?? 'Nome não prenchido'; // ! checando com null aware operator
   print(nomeCompleto?.toUpperCase() ??
-      'Nome não preenchido'); // checando com conditional property access
-}  // só vai executar a "função", se a variável não for nula
-   // se ela for nula, vai ignorar a "função" 
+      'Nome não preenchido'); // ! checando com conditional property access
+}  // ! só vai executar a "função", se a variável não for nula
+   // ! se ela for nula, vai ignorar a "função" 
   
 
   // var nomeCompletoLocal = nomeCompleto;
