7d8958f280eee98289a9dfe141cd0078bfe2a39f
Carlos Henrique <chenrilima.ch@gmail.com>
2022-07-25 07:01:17 -0300

    chore: add comments

--- lib/4_tratamento_nulos/4_conditional_property_access.dart
+++ lib/4_tratamento_nulos/4_conditional_property_access.dart
@@ -4,7 +4,8 @@ void main() {
   // var nomeCompletoLocal = nomeCompleto  ?? 'Nome não prenchido'; // checando com null aware operator
   print(nomeCompleto?.toUpperCase() ??
       'Nome não preenchido'); // checando com conditional property access
-}  
+}  // só vai executar a "função", se a variável não for nula
+   // se ela for nula, vai ignorar a "função" 
   
 
   // var nomeCompletoLocal = nomeCompleto;
